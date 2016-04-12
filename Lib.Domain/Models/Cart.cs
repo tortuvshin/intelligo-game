76c42a277be06289f3f7475015e1ee33881860a7
tortuvshin <toroo.byamba@gmail.com>
2016-04-12 23:39:40 +0900

    #11 Book, Cart, CartBook Model-ууд үүсгэв

---
+++ Lib.Domain/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lib.Domain.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public virtual ICollection<CartBook> CartBooks { get; set; }
+    }
+}
