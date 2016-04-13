24dc2951c8c6851d0171bb80a7892d89d1bc1dd3
tortuvshin <toroo.byamba@gmail.com>
2016-04-13 23:49:46 +0900

    packages up

---
+++ Lib/Controllers/Common.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lib.Controllers
+{
+    public class Common : Controller
+    { 
+        //[Inject]
+        //public ILogger Logger { get; set; }
+        public string GetUserId()
+        {
+            return User.Identity.GetUserId();
+        }
+
+        public string GetUserName()
+        {
+            return User.Identity.GetUserName();
+        }
+
+        public string SaveOnServer(string fileName, string key)
+        {
+            var filePath = string.Empty;
+            try
+            {
+                filePath = Path.Combine(Server.MapPath(Keys.PathUploads), Path.GetFileName(fileName));
+                Request.Files[key].SaveAs(filePath);
+            }
+            catch (Exception e)
+            {
+        //        Logger.Error(e);
+            }
+            return filePath;
+        }
+
+    }
+}
