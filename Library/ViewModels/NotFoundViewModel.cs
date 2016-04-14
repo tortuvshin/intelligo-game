5328c026ffc45d59915e67bbe4049ff576435456
tortuvshin <toroo.byamba@gmail.com>
2016-04-14 09:17:30 +0900

    Book view manage book

---
+++ Library/ViewModels/NotFoundViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Itera.Fagdag.November.ViewModels
+{
+    public class NotFoundViewModel
+    {
+        public string ElementName { get; set; }
+    }
+}
