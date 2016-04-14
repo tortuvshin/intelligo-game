fc46631a569ebbc05434c7a19610c7ebc9deec3c
tortuvshin <toroo.byamba@gmail.com>
2016-04-14 09:20:11 +0900

    updated

---
+++ Library/Data/IteraLibraryDbContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Itera.Fagdag.November.Domain.Models;
+
+namespace Itera.Fagdag.November.Data
+{
+    public class IteraLibraryDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+
+            // Add any configuration or mapping stuff here
+
+            //modelBuilder.Entity<Cart>().HasMany(c => c.CartBooks).WithOptional().Map(m => m.MapKey("CartBookId"));
+        }
+
+        public void Seed(IteraLibraryDbContext dbContext)
+        {
+            #if DEBUG
+            // Create my debug (testing) objects here
+            #endif
+            // Normal seeding goes here
+            SeedData.SeedBooks(dbContext);
+        }
+
+        public class DropCreateAlwaysInitializer : DropCreateDatabaseAlways<IteraLibraryDbContext>
+        {
+            protected override void Seed(IteraLibraryDbContext dbContext)
+            {
+                dbContext.Seed(dbContext);
+
+                base.Seed(dbContext);
+            }
+        }
+
+        public class DropCreateIfChangeInitializer : DropCreateDatabaseIfModelChanges<IteraLibraryDbContext>
+        {
+            protected override void Seed(IteraLibraryDbContext dbContext)
+            {
+                dbContext.Seed(dbContext);
+
+                base.Seed(dbContext);
+            }
+        }
+
+        public class CreateInitializer : CreateDatabaseIfNotExists<IteraLibraryDbContext>
+        {
+            protected override void Seed(IteraLibraryDbContext dbContext)
+            {
+                dbContext.Seed(dbContext);
+
+                base.Seed(dbContext);
+            }
+        }
+
+        static IteraLibraryDbContext()
+        {
+            #if DEBUG
+            //Database.SetInitializer(new DropCreateAlwaysInitializer());
+            Database.SetInitializer(new DropCreateIfChangeInitializer());
+            #else
+            Database.SetInitializer(new CreateInitializer());
+            #endif
+        }
+    }
+}
---
+++ Library/Helpers/Conversion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Itera.Fagdag.November.Helpers
+{
+    public static class Conversion
+    {
+        public static short ToInt16(this object obj)
+        {
+            try
+            {
+                var val = Convert.ToInt16(obj.ToString());
+                return val;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public static int ToInt32(this object obj)
+        {
+            try
+            {
+                var val = Convert.ToInt32(obj.ToString());
+                return val;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        public static long ToInt64(this object obj)
+        {
+            try
+            {
+                var val = Convert.ToInt64(obj.ToString());
+                return val;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
