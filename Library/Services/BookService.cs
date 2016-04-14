978adb80f67007da8123c863055059c6116c1f30
tortuvshin <toroo.byamba@gmail.com>
2016-04-14 09:21:06 +0900

    Book service completed

---
+++ Library/Services/BookService.cs
@@ -0,0 +1,45 @@
+﻿using Itera.Fagdag.November.Domain.Models;
+using Itera.Fagdag.November.Repositories.Contracts;
+using Itera.Fagdag.November.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Itera.Fagdag.November.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+        
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return _bookRepository.GetBooks();
+        }
+        
+        public Book GetBook(int id)
+        {
+            return _bookRepository.GetBook(id);
+        }
+
+        public void Update(Book book)
+        {
+            _bookRepository.Update(book);
+        }
+
+        public void Remove(Book book)
+        {
+            _bookRepository.Remove(book);
+        }
+
+        public void Add(Book book)
+        {
+            _bookRepository.Add(book);
+        }
+    }
+}
---
+++ Library/Services/Contracts/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using Itera.Fagdag.November.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itera.Fagdag.November.Services.Contracts
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetBooks();
+        Book GetBook(int id);
+        void Update(Book book);
+        void Remove(Book book);
+        void Add(Book book);
+    }
+}
