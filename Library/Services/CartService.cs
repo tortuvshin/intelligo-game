304a49cce613bb1b922780134be540009eeda154
tortuvshin <toroo.byamba@gmail.com>
2016-04-14 09:15:35 +0900

    card service, model completed

---
+++ Library/Services/CartService.cs
@@ -0,0 +1,39 @@
+﻿using Itera.Fagdag.November.Domain.Models;
+using Itera.Fagdag.November.Repositories.Contracts;
+using Itera.Fagdag.November.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Itera.Fagdag.November.Services
+{
+    public class CartService : ICartService
+    {
+        private readonly ICartRepository _cartRepository;
+        
+        public CartService(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public void InsertCart(Cart cart)
+        {
+            _cartRepository.InsertCart(cart);
+        }
+
+        public Cart GetCart(int id)
+        {
+            return _cartRepository.GetCart(id);
+        }
+        public Cart GetCartByUserId(string userId)
+        {
+            return _cartRepository.GetCartByUserId(userId);
+        }
+
+        public void UpdateCart(Cart cart)
+        {
+            _cartRepository.UpdateCart(cart);
+        }
+    }
+}
---
+++ Library/ViewModels/CartViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Itera.Fagdag.November.Domain.Models;
+
+namespace Itera.Fagdag.November.ViewModels
+{
+    public class CartViewModel
+    {
+        public ICollection<CartBook> CartBooks { get; set; }
+
+        public string CartText
+        {
+            get
+            {
+                if (CartBooks.Count == 0)
+                {
+                    return "Цуглуулга хоосон байна.";
+                }
+                return string.Empty;
+            }
+        }
+    }
+}
