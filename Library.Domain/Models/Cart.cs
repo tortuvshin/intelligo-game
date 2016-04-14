f45c494ce88e16ab5258e3c7fac4e0fab78014e1
tortuvshin <toroo.byamba@gmail.com>
2016-04-14 09:16:58 +0900

    card , cardbook , account model

---
+++ Library.Domain/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Itera.Fagdag.November.Domain.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public virtual ICollection<CartBook> CartBooks { get; set; }
+    }
+}
