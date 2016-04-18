42c30ac0d922019b1ee76c48a7124650ed7d5717
tortuvshin <toroo.byamba@gmail.com>
2016-04-19 02:16:00 +0900

    BooksController created

--- src/Library/Models/ApplicationDbContext.cs
+++ src/Library/Models/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Data.Entity;
+using Library.Models;
 
 namespace Library.Models
 {
@@ -16,5 +17,6 @@ namespace Library.Models
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+        public DbSet<Book> Book { get; set; }
     }
 }
