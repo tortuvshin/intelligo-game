54ac8dcc2f370e4f35d489d820476a65b4ae6c90
tortuvshin <toroo.byamba@gmail.com>
2016-04-19 03:04:33 +0900

    first migration completed

--- src/Library/Migrations/20160418180153_Initial.Designer.cs
+++ src/Library/Migrations/20160418180153_Initial.Designer.cs
@@ -8,9 +8,10 @@ using Library.Models;
 namespace Library.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160418180153_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
