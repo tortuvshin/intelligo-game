cce8ff5ea39a403481d9afe3f7a10a978012dcd5
tortuvshin <toroo.byamba@gmail.com>
2016-04-19 01:33:00 +0900

    Author and Book models created

---
+++ src/Library/Models/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Author
+    {
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ src/Library/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        [ScaffoldColumn(false)]
+        public int BookID { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+        [Range(1, 500)]
+        public decimal Price { get; set; }
+
+        public string Genre { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+
+        // Navigation property
+        public virtual Author Author { get; set; }
+    }
+}
