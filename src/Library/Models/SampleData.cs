876fcb0c8ebfe8bd0dbc285ce78c9d42f30357b0
tortuvshin <toroo.byamba@gmail.com>
2016-04-25 10:44:33 +0900

    book updated

--- src/Library/Models/SampleData.cs
+++ src/Library/Models/SampleData.cs
@@ -22,40 +22,6 @@ namespace Library.Models
                 var cervantes = context.Author.Add(
                     new Author { LastName = "Cervantes", FirstMidName = "Miguel" }).Entity;
 
-                context.Book.AddRange(
-                    new Book()
-                    {
-                        Title = "Pride and Prejudice",
-                        Year = 1813,
-                        Author = austen,
-                        Price = 9.99M,
-                        Genre = "Comedy of manners"
-                    },
-                    new Book()
-                    {
-                        Title = "Northanger Abbey",
-                        Year = 1817,
-                        Author = austen,
-                        Price = 12.95M,
-                        Genre = "Gothic parody"
-                    },
-                    new Book()
-                    {
-                        Title = "David Copperfield",
-                        Year = 1850,
-                        Author = dickens,
-                        Price = 15,
-                        Genre = "Bildungsroman"
-                    },
-                    new Book()
-                    {
-                        Title = "Don Quixote",
-                        Year = 1617,
-                        Author = cervantes,
-                        Price = 8.95M,
-                        Genre = "Picaresque"
-                    }
-                );
                 context.SaveChanges();
             }
         }
