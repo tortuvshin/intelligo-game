a8d159bb822f1c0a731d2120d66884fc3dd434be
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-05-31 23:20:28 +0900

    morgan added, base route get game view

--- app.js
+++ app.js
@@ -4,20 +4,12 @@ const express = require( 'express'),
       expressLayouts = require('express-ejs-layouts'),
       path           = require('path'),
       morgan         = require('morgan'),
-      bodyParser     = require('body-parser'),
-      mongoose       = require('mongoose'),
-      cookieParser   = require('cookie-parser'),
-      config         = require('config'),
-      session        = require('express-session'),
-      flash          = require('connect-flash'),
-      expressValidator = require('express-validator'),
-      passport       = require("passport");
+      bodyParser     = require('body-parser');
 
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
 app.use(morgan('dev'));
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -25,7 +17,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-
+app.get('/', function(req, res) {
+    res.render('game.ejs');
+});
 app.listen(app.get('port'), () => {
     console.log('Intelligo game is listening on port', app.get('port'));
 });
