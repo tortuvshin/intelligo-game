9f0bfb2cbab5c1a09c43229abc677cf1a03d8184
Doljko <doljko_tv@yahoo.com>
2018-07-03 16:52:18 +0900

    rank.ejs created

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.get('/', function(req, res) {
-    res.render('user.ejs');
+app.get('/rank', function(req, res) {
+    res.render('rank.ejs');
 });
 app.listen(app.get('port'), () => {
     console.log('Intelligo game is listening on port', app.get('port'));
