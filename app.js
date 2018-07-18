eeac8afffa47bf8e8a1a3c90e91cc38fefaa880a
Doljinsuren Enkhbayar <doljko927@gmail.com>
2018-07-19 01:02:36 +0900

    css үүд цэгцэлж sass болгох #9

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/demo', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/intelligo', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
